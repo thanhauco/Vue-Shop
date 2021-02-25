7b726a08c01657a71128a58112969f17c9056ed5
Thanhmsft <v-thanhvu@microsoft.com>
2021-02-25 13:20:00 -0800

    feat(icons): Add navigation icons

---
+++ src/components/icons/IconArrowDown.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import IconBase from './IconBase.vue'
+</script>
+
+<template>
+  <IconBase v-bind="$attrs">
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <polyline points="19 12 12 19 5 12"></polyline>
+  </IconBase>
+</template>
---
+++ src/components/icons/IconArrowLeft.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import IconBase from './IconBase.vue'
+</script>
+
+<template>
+  <IconBase v-bind="$attrs">
+    <line x1="19" y1="12" x2="5" y2="12"></line>
+    <polyline points="12 19 5 12 12 5"></polyline>
+  </IconBase>
+</template>
