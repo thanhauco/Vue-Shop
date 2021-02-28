640a9d2aeb7d60f2c7e59824e80932505aebf441
Thanhmsft <v-thanhvu@microsoft.com>
2021-02-28 09:20:00 -0800

    feat(icons): Add action icons

---
+++ src/components/icons/IconPlus.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import IconBase from './IconBase.vue'
+</script>
+
+<template>
+  <IconBase v-bind="$attrs">
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <line x1="5" y1="12" x2="19" y2="12"></line>
+  </IconBase>
+</template>
