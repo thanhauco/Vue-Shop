facbd293b0c54745c188af455064f2754a17dfae
Kevin <kevin@wmiworldwide.com>
2021-03-04 13:15:00 -0800

    feat(icons): Add chevron and menu icons

---
+++ src/components/icons/IconChevronRight.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import IconBase from './IconBase.vue'
+</script>
+
+<template>
+  <IconBase v-bind="$attrs">
+    <polyline points="9 18 15 12 9 6"></polyline>
+  </IconBase>
+</template>
