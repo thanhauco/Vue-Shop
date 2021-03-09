c1fad574b999e18fad09545d567e413d3d1bcbfb
Thanhmsft <v-thanhvu@microsoft.com>
2021-03-09 12:15:00 -0800

    feat(icons): Add media device icons

---
+++ src/components/icons/IconMonitor.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import IconBase from './IconBase.vue'
+</script>
+
+<template>
+  <IconBase v-bind="$attrs">
+    <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+    <line x1="8" y1="21" x2="16" y2="21"></line>
+    <line x1="12" y1="17" x2="12" y2="21"></line>
+  </IconBase>
+</template>
