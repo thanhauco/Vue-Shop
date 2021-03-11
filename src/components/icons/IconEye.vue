8694006f514c78d31242312659caab568aeaff79
Thanhmsft <v-thanhvu@microsoft.com>
2021-03-10 16:51:00 -0800

    feat(icons): Add security icons

---
+++ src/components/icons/IconEye.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import IconBase from './IconBase.vue'
+</script>
+
+<template>
+  <IconBase v-bind="$attrs">
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+  </IconBase>
+</template>
