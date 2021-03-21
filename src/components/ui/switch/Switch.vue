5042e1d66a22c0f441fecd3ea25fce89a53bcd38
thanh <thanhauco@gmail.com>
2021-03-21 10:49:00 -0700

    feat(ui): Add Textarea and Switch

---
+++ src/components/ui/switch/Switch.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+interface Props {
+  modelValue?: boolean
+  disabled?: boolean
+}
+
+defineProps<Props>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void
+}>()
+</script>
+
+<template>
+  <button 
+    class="n-switch" 
+    :class="{ checked: modelValue, disabled }" 
+    @click="!disabled && emit('update:modelValue', !modelValue)"
+    type="button"
+  >
+    <span class="n-switch-toggle"></span>
+  </button>
+</template>
+
+<style scoped>
+.n-switch {
+  width: 44px;
+  height: 24px;
+  background-color: var(--color-surface);
+  border: 1px solid var(--color-border);
+  border-radius: 9999px;
+  position: relative;
+  transition: all 0.2s;
+  cursor: pointer;
+  padding: 2px;
+}
+
+.n-switch:focus {
+  outline: none;
+  box-shadow: 0 0 0 2px rgba(99, 102, 241, 0.2);
+}
+
+.n-switch.checked {
+  background-color: var(--color-primary);
+  border-color: var(--color-primary);
+}
+
+.n-switch.disabled {
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+
+.n-switch-toggle {
+  display: block;
+  width: 18px;
+  height: 18px;
+  background-color: white;
+  border-radius: 50%;
+  transition: transform 0.2s;
+  box-shadow: 0 1px 2px rgba(0,0,0,0.2);
+}
+
+.n-switch.checked .n-switch-toggle {
+  transform: translateX(20px);
+}
+</style>
---
+++ src/components/ui/textarea/Textarea.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+interface Props {
+  modelValue?: string
+  placeholder?: string
+  rows?: number
+  disabled?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  modelValue: '',
+  rows: 4,
+  disabled: false
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const onInput = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLTextAreaElement).value)
+}
+</script>
+
+<template>
+  <textarea
+    class="n-textarea"
+    :value="modelValue"
+    :placeholder="placeholder"
+    :rows="rows"
+    :disabled="disabled"
+    @input="onInput"
+  ></textarea>
+</template>
+
+<style scoped>
+.n-textarea {
+  width: 100%;
+  padding: 0.625rem 1rem;
+  background-color: var(--color-surface);
+  border: 1px solid var(--color-border);
+  border-radius: var(--radius-md);
+  color: var(--color-text-main);
+  font-family: inherit;
+  font-size: var(--font-size-sm);
+  resize: vertical;
+  transition: all 0.2s;
+}
+
+.n-textarea:focus {
+  outline: none;
+  border-color: var(--color-primary);
+  box-shadow: 0 0 0 2px rgba(99, 102, 241, 0.2);
+}
+
+.n-textarea:disabled {
+  opacity: 0.6;
+  cursor: not-allowed;
+  background-color: rgba(0,0,0,0.2);
+}
+</style>
