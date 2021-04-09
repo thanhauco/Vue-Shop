8cb73f5e8f69de1792be1983f2810d485fc340b2
thanh <thanhauco@gmail.com>
2021-04-09 09:12:00 -0700

    feat(ui): Add Progress and Skeleton

---
+++ src/components/ui/progress/ProgressBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface Props {
+  progress: number
+  color?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  color: 'var(--color-primary)'
+})
+</script>
+
+<template>
+  <div class="n-progress-bar">
+    <div 
+      class="fill" 
+      :style="{ width: `${progress}%`, backgroundColor: color }"
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+.n-progress-bar {
+  width: 100%;
+  height: 8px;
+  background-color: var(--color-surface-hover);
+  border-radius: var(--radius-full);
+  overflow: hidden;
+}
+
+.fill {
+  height: 100%;
+  border-radius: var(--radius-full);
+  transition: width 0.3s ease;
+}
+</style>
