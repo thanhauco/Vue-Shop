49866575dc6043c7f21a637d58605b8825b92512
Kevin <kevin@wmiworldwide.com>
2021-04-19 13:30:00 -0700

    feat(core): Add UI interactions hooks

---
+++ src/directives/vClickOutside.ts
@@ -0,0 +1,13 @@
+export const vClickOutside = {
+  mounted(el: any, binding: any) {
+    el.clickOutsideEvent = (event: Event) => {
+      if (!(el === event.target || el.contains(event.target))) {
+        binding.value(event)
+      }
+    }
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unmounted(el: any) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  }
+}
