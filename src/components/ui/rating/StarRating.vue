ace2337133a4f8af62865901f0a8663a56e04187
thanh <thanhauco@gmail.com>
2021-05-07 20:30:00 -0700

    feat(reviews): Add StarRating component

---
+++ src/components/ui/rating/StarRating.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ rating: number }>()
+</script>
+<template>
+  <div class="star-rating">
+    <span v-for="i in 5" :key="i" :class="{ filled: i <= rating }">★</span>
+  </div>
+</template>
+<style scoped>
+.star-rating { color: gold; }
+.filled { color: orange; }
+</style>
