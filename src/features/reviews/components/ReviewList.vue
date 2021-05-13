25a96a235b0388748f5efe22eec6be25026fdf84
thanh <thanhauco@gmail.com>
2021-05-13 10:30:00 -0700

    feat(reviews): Add ReviewList component

---
+++ src/features/reviews/components/ReviewList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="review-list">
+    <div v-for="review in reviews" :key="review.id" class="review-item">
+      <h3>{{ review.author }}</h3>
+      <p>{{ review.content }}</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{ reviews: any[] }>()
+</script>
