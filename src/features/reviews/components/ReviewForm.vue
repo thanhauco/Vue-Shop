48c09ad39787ee6453149b133143858a047243a3
Thanhmsft <v-thanhvu@microsoft.com>
2021-05-20 22:30:00 -0700

    feat(reviews): Add ReviewForm component

---
+++ src/features/reviews/components/ReviewForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <form @submit.prevent="submit">
+    <textarea v-model="content"></textarea>
+    <button type="submit">Submit Review</button>
+  </form>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const content = ref('')
+const submit = () => { /* TODO */ }
+</script>
