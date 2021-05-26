0c26a43fdb3b4999fea468176f1d23ef77ad6c40
Thanhmsft <v-thanhvu@microsoft.com>
2021-05-26 11:30:00 -0700

    feat(blog): Add BlogPost component

---
+++ src/features/blog/components/BlogPost.vue
@@ -0,0 +1,9 @@
+<template>
+  <article class="blog-post">
+    <h1>{{ title }}</h1>
+    <div class="content"><slot /></div>
+  </article>
+</template>
+<script setup lang="ts">
+defineProps<{ title: string }>()
+</script>
