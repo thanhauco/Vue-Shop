d6efe284b247441ec09603031a55ed98915d95a6
Thanhmsft <v-thanhvu@microsoft.com>
2021-06-07 11:30:00 -0700

    feat(settings): Add Notifications Settings

---
+++ src/features/settings/pages/NotificationSettings.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="settings-notifications">
+    <h2>Email Notifications</h2>
+    <label><input type="checkbox" /> Order Updates</label>
+    <label><input type="checkbox" /> Promotions</label>
+  </div>
+</template>
