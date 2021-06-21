1ed359eec82eef1d599b19b5a69e315478265877
Thanhmsft <v-thanhvu@microsoft.com>
2021-06-21 13:30:00 -0700

    feat(layout): Add Footer component

---
+++ src/layouts/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="app-footer">
+    <div class="container">
+      <p>&copy; 2021 Vue Nebula Shop</p>
+    </div>
+  </footer>
+</template>
+<style scoped>
+.app-footer { padding: 2rem; background: #eee; }
+</style>
