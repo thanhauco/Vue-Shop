6407d82e5796131269dcf6610638d6ca1d027158
thanh <thanhauco@gmail.com>
2021-06-22 10:11:00 -0700

    feat(user): Add Wishlist

---
+++ src/features/user/pages/WishlistPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import ProductGrid from '@/features/products/components/ProductGrid.vue' // Reusing existing component
+import { useProductStore } from '@/features/products/stores/productStore'
+
+const productStore = useProductStore()
+// Mocking wishlist by just creating a dummy list of product IDs and filtering
+const wishlistIds = ref(['1', '3'])
+</script>
+
+<template>
+  <div class="wishlist-page">
+    <h1>My Wishlist</h1>
+    <p v-if="wishlistIds.length === 0">Your wishlist is empty.</p>
+    <!-- In a real app we would filter products or fetch wishlist items -->
+    <ProductGrid v-else /> 
+  </div>
+</template>
