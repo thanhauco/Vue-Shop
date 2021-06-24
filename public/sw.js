962b2454e3bbb8052117479ade4908a1490705bb
Thanhmsft <v-thanhvu@microsoft.com>
2021-06-24 10:48:00 -0700

    feat(pwa): Add Manifest

---
+++ public/sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'nebula-shop-v1'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+]
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache')
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response
+        }
+        return fetch(event.request).then(response => {
+          // Don't cache non-successful responses
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response
+          }
+          const responseToCache = response.clone()
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache)
+            })
+          return response
+        })
+      })
+  )
+})
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME]
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
