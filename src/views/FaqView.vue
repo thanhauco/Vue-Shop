64ce0eca77bd80dcc89d2b66159988e9b622a270
thanh <thanhauco@gmail.com>
2021-07-01 22:30:00 -0700

    feat(pages): Add FAQ page

---
+++ src/views/FaqView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="faq-page">
+    <h1>Frequently Asked Questions</h1>
+    <details>
+      <summary>Shipping?</summary>
+      <p>We ship globally.</p>
+    </details>
+  </div>
+</template>
