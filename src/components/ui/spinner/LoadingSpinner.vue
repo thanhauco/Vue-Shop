cb9ec340217ff8ea2abd9b2c27c06fd860a0b4e9
thanh <thanhauco@gmail.com>
2021-07-20 05:30:00 -0700

    ui(components): Add LoadingSpinner

---
+++ src/components/ui/spinner/LoadingSpinner.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="spinner"></div>
+</template>
+<style>
+.spinner { width: 20px; height: 20px; border: 2px solid #ccc; border-top-color: #333; border-radius: 50%; animation: spin 1s infinite; }
+@keyframes spin { to { transform: rotate(360deg); } }
+</style>
