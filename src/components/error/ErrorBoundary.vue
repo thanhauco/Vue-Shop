99798e8a37f2e98a7039576f32a4ab8d362aae6c
Thanhmsft <v-thanhvu@microsoft.com>
2021-08-19 01:30:00 -0700

    feat(error): Add ErrorBoundary component

---
+++ src/components/error/ErrorBoundary.vue
@@ -0,0 +1,9 @@
+<template>
+  <slot v-if="!error" />
+  <div v-else class="error">Something went wrong.</div>
+</template>
+<script setup lang="ts">
+import { onErrorCaptured, ref } from 'vue'
+const error = ref(null)
+onErrorCaptured(err => { error.value = err; return false })
+</script>
